8bb81a83d76e47c339cb06d17440ea563959de9c
GuKBABjOa <128679235+GuKBABjOa@users.noreply.github.com>
2024-05-25 21:57:18 +0900

    5/25

---
+++ I_O_Prac/src/i_o/io8393.java
@@ -0,0 +1,18 @@
+package i_o;
+import java.util.Scanner;
+public class io8393 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        
+        System.out.println(sum);
+
+	}
+
+}
